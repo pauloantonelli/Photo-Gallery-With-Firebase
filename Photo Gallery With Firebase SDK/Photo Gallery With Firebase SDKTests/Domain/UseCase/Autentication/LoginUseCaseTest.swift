beb9e94536a96ff774477ed041e78d0d9adcbfd3
pauloantonelli <pauloantonelli@hotmail.com>
2022-10-29 12:58:19 -0300

    login use case test in progress

--- Photo Gallery With Firebase SDK/Photo Gallery With Firebase SDKTests/Domain/UseCase/Autentication/LoginUseCaseTest.swift
+++ Photo Gallery With Firebase SDK/Photo Gallery With Firebase SDKTests/Domain/UseCase/Autentication/LoginUseCaseTest.swift
@@ -8,24 +8,45 @@
 import XCTest
 @testable import Photo_Gallery_With_Firebase_SDK
 
-class LoginUseCaseTest: XCTestCase {
-    var repository: LoginRepository
-    var usecase: LoginUseCase
+struct FirebaseMock: IFirebaseService {
+    let user: User = User(id: "user123", username: "Paulo Antonelli", photoUrl: URL(string: "https://minhafoto.com/pauloantonelli")!)
     
-    override init() {
-        super.init()
-        self.repository = LoginRepository(datasource: )
+    func login(email: String, password: String) async throws -> User {
+        return self.user
+    }
+    
+    func register(email: String, password: String) async throws -> User {
+        return self.user
+    }
+}
+class LoginUseCaseTest: XCTestCase {
+    var firebaseService: IFirebaseService!
+    var datasource: LoginDataSource!
+    var repository: LoginRepository!
+    var usecase: LoginUseCase!
+    let email: Email = Email(email: "pauloantonelli@zoominitcode.dev")
+    let password: Password = Password(password: "abc123")
+    
+    func initializeDependency() -> Void {
+        self.firebaseService = FirebaseMock()
+        self.datasource = LoginDataSource(firebase: self.firebaseService)
+        self.repository = LoginRepository(datasource: datasource)
         self.usecase = LoginUseCase(repository: repository)
     }
     
     override func setUpWithError() throws {
-        
+        self.initializeDependency()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testLoginWithoutErrors() async throws {
+        let credential: Credential = Credential(email: self.email, password: self.password)
+        let result = await self.usecase.execute(withCredential: credential)
+        XCTAssert(try result.get() is User)
     }
-
-    func testExample() throws {
+    
+    func testLoginWithEmailError() async throws {
+        let credential: Credential = Credential(email: Email(), password: self.password)
+        let result = await self.usecase.execute(withCredential: credential)
+        XCTAssert(try result is LoginErrorUseCase)
     }
 }
