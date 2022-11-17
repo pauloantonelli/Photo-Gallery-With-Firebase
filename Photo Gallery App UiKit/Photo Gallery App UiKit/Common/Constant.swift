e01db8b37b9e3cc20f17627786d20e9dbff758bf
pauloantonelli <pauloantonelli@hotmail.com>
2022-11-17 17:01:41 -0300

    Uikit App Screens in progress

---
+++ Photo Gallery App UiKit/Photo Gallery App UiKit/Common/Constant.swift
@@ -0,0 +1,8 @@
+//
+//  Constant.swift
+//  Photo Gallery App UiKit
+//
+//  Created by Paulo Antonelli on 15/11/22.
+//
+
+import Foundation
---
+++ Photo Gallery App UiKit/Photo Gallery App UiKit/Gallery/GalleryCell/GalleryCellCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryCellCollectionViewCell.swift
+//  Photo Gallery App UiKit
+//
+//  Created by Paulo Antonelli on 15/11/22.
+//
+
+import UIKit
+
+class GalleryCellCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
