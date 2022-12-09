4d0805368c03929cf1e1b36186bff4bca9d92650
pauloantonelli <pauloantonelli@hotmail.com>
2022-12-09 16:25:47 -0300

    gallery grid finalized

---
+++ Photo Gallery App UiKit/Photo Gallery App UiKit/Gallery/GalleryHeader/GalleryHeaderCollectionReusableView.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryHeaderCollectionReusableView.swift
+//  Photo Gallery App UiKit
+//
+//  Created by Paulo Antonelli on 09/12/22.
+//
+
+import UIKit
+
+class GalleryHeaderCollectionReusableView: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
