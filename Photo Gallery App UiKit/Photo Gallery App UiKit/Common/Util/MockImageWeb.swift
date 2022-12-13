c74fb5da2943d1787774b107ceeaa54445ff46da
pauloantonelli <pauloantonelli@hotmail.com>
2022-12-13 12:16:16 -0300

    UIkit App done

---
+++ Photo Gallery App UiKit/Photo Gallery App UiKit/Common/Util/MockImageWeb.swift
@@ -0,0 +1,8 @@
+//
+//  MockImageWeb.swift
+//  Photo Gallery App UiKit
+//
+//  Created by Paulo Antonelli on 13/12/22.
+//
+
+import Foundation
---
+++ Photo Gallery App UiKit/Photo Gallery App UiKit/Common/Util/StringExtension.swift
@@ -0,0 +1,12 @@
+//
+//  ExtractStringExtension.swift
+//  Photo Gallery App UiKit
+//
+//  Created by Paulo Antonelli on 13/12/22.
+//
+
+import Foundation
+
+extension String {
+    func extractExactString
+}
