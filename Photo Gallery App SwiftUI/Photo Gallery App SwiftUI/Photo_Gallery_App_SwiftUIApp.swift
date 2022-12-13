6e17516424a07cae4b42877f4abac82816d7ee4c
pauloantonelli <pauloantonelli@hotmail.com>
2022-12-13 12:20:46 -0300

    swift ui app created

---
+++ Photo Gallery App SwiftUI/Photo Gallery App SwiftUI/Photo_Gallery_App_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_Gallery_App_SwiftUIApp.swift
+//  Photo Gallery App SwiftUI
+//
+//  Created by Paulo Antonelli on 13/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Photo_Gallery_App_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Photo Gallery App SwiftUI/Photo Gallery App SwiftUIUITests/Photo_Gallery_App_SwiftUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Photo_Gallery_App_SwiftUIUITestsLaunchTests.swift
+//  Photo Gallery App SwiftUIUITests
+//
+//  Created by Paulo Antonelli on 13/12/22.
+//
+
+import XCTest
+
+class Photo_Gallery_App_SwiftUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
