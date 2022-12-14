f54db5a68ed402a7479cf7eec70607682e6f6e89
pauloantonelli <pauloantonelli@hotmail.com>
2022-12-14 17:40:24 -0300

    permition page in progress

--- Photo Gallery App SwiftUI/Photo Gallery App SwiftUI/Permission/PermissionView.swift
+++ Photo Gallery App SwiftUI/Photo Gallery App SwiftUI/Permission/PermissionView.swift
@@ -9,7 +9,43 @@ import SwiftUI
 
 struct PermissionView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image("permission")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(minHeight: 0.0, maxHeight: 150.0, alignment: .center)
+            Text("Allow your Camera")
+            Text("We will need your camera to give you better experience.")
+            Button("Sure, I'd Like that", action: {
+                self.grantPermition(status: true)
+            })
+            .frame(
+                minWidth: 0,
+                maxWidth: .infinity,
+                maxHeight: 35.0
+            )
+            .foregroundColor(Color("PermissionButtonLabelColor"))
+            .background(Color("PermissionButtonBackgroundColor"))
+            .cornerRadius(5.0)
+            .padding(.bottom, 10.0)
+            Button("Not now", action: {
+                self.grantPermition(status: false)
+            })
+            .frame(
+                minWidth: 0,
+                maxWidth: .infinity,
+                maxHeight: 35.0
+            )
+            .foregroundColor(Color("PermissionButtonBackgroundColor"))
+            .background(Color("PermissionButtonLabelColor"))
+            .cornerRadius(5.0)
+            .padding(.bottom, 10.0)
+        }
+        .padding(.horizontal, 20.0)
+    }
+    
+    func grantPermition(status: Bool) -> Void {
+        
     }
 }
 
