b96bcda7ae9701dc789503f370526c03018085fc
pauloantonelli <pauloantonelli@hotmail.com>
2022-12-14 17:40:21 -0300

    permition page in progress

---
+++ Photo Gallery App SwiftUI/Photo Gallery App SwiftUI/Permission/PermissionView.swift
@@ -0,0 +1,20 @@
+//
+//  PermissionView.swift
+//  Photo Gallery App SwiftUI
+//
+//  Created by Paulo Antonelli on 14/12/22.
+//
+
+import SwiftUI
+
+struct PermissionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PermissionView_Previews: PreviewProvider {
+    static var previews: some View {
+        PermissionView()
+    }
+}
